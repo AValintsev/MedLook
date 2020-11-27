166ea695b2ff3bb6a5de161bea958fda9ee90646
Anton Valintsev <valintsev.anton@gmail.com>
2020-11-27 17:30:50 +0200

    main page, home page products

---
+++ Presentation/Nop.Web/Themes/MedLook/Content/js/carousel.default.js
@@ -0,0 +1,33 @@
+﻿new Glider(document.querySelector('.glider'), {
+  slidesToShow: 1,
+  dots: '#dots',
+  draggable: true,
+  scrollLock: true,
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  },
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 475,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
