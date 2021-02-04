e96571f4a5f3b3d9566f8b8e26a9ecbe5cb33d84
Anton Valintsev <valintsev.anton@gmail.com>
2021-02-04 15:24:03 +0200

    change styling

--- Presentation/Nop.Web/Themes/MedLook/Content/js/carousel.default.js
+++ Presentation/Nop.Web/Themes/MedLook/Content/js/carousel.default.js
@@ -1,33 +1,35 @@
-﻿new Glider(document.querySelector('.glider'), {
-  slidesToShow: 1,
-  dots: '#dots',
-  draggable: true,
-  scrollLock: true,
-  arrows: {
-    prev: '.glider-prev',
-    next: '.glider-next'
-  },
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 900,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 475,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
+﻿if (document.querySelector('.glider')) {
+	new Glider(document.querySelector('.glider'), {
+		slidesToShow: 1,
+		dots: '#dots',
+		draggable: true,
+		scrollLock: true,
+		arrows: {
+			prev: '.glider-prev',
+			next: '.glider-next'
+		},
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 900,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 475,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+}
