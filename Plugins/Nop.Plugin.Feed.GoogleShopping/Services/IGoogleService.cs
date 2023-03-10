50f3337ed61d9d63f7998e4c145d3f95f81825e2
Anton Valintsev <valintsev.anton@gmail.com>
2023-03-10 17:40:12 +0200

    added google feed plugin

---
+++ Plugins/Nop.Plugin.Feed.GoogleShopping/Services/IGoogleService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nop.Plugin.Feed.GoogleShopping.Domain;
+
+namespace Nop.Plugin.Feed.GoogleShopping.Services
+{
+    public partial interface IGoogleService
+    {
+        Task DeleteGoogleProductAsync(GoogleProductRecord googleProductRecord);
+
+        Task<IList<GoogleProductRecord>> GetAllAsync();
+
+        Task<GoogleProductRecord> GetByIdAsync(int googleProductRecordId);
+
+        Task<GoogleProductRecord> GetByProductIdAsync(int productId);
+
+        Task InsertGoogleProductRecordAsync(GoogleProductRecord googleProductRecord);
+
+        Task UpdateGoogleProductRecordAsync(GoogleProductRecord googleProductRecord);
+
+        Task<IList<string>> GetTaxonomyListAsync();
+    }
+}
