5d9948f1e16567027230c4effe79e67a4d6ed4e1
Anton Valintsev <valintsev.anton@gmail.com>
2024-02-16 14:18:05 +0200

    update address.address1 with info from nova poshta

---
+++ Plugins/Nop.Ithoot.Plugin.Shipping.NovaPoshta/Infrastructure/Events/AddressInservtedEventConsumer.cs
@@ -0,0 +1,36 @@
+﻿using Nop.Core.Domain.Common;
+using Nop.Core.Events;
+using Nop.Data;
+using Nop.Services.Common;
+using Nop.Services.Events;
+using System.Threading.Tasks;
+
+namespace Nop.Ithoot.Plugin.Shipping.NovaPoshta.Infrastructure.Events
+{
+    public class AddressInservtedEventConsumer : IConsumer<EntityInsertedEvent<Address>>
+    {
+        private readonly INopDataProvider _dataProvider;
+        private readonly IAddressAttributeFormatter _addressAttributeFormatter;
+
+        public AddressInservtedEventConsumer(
+            INopDataProvider dataProvider,
+            IAddressAttributeFormatter addressAttributeFormatter)
+        {
+            _dataProvider = dataProvider;
+            _addressAttributeFormatter = addressAttributeFormatter;
+        }
+
+        public async Task HandleEventAsync(EntityInsertedEvent<Address> eventMessage)
+        {
+            var address = eventMessage.Entity;
+
+            if (!string.IsNullOrEmpty(address.CustomAttributes))
+            {
+                var attrs = await _addressAttributeFormatter.FormatAttributesAsync(address.CustomAttributes, " ");
+                address.Address1 = attrs;
+                
+                await _dataProvider.UpdateEntityAsync(address);
+            }
+        }
+    }
+}
